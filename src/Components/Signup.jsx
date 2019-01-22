9d734124a7449b93ed7a69470a9142e1be28da68
Ryan Brockley <rybrockers@yahoo.com>
2019-01-22 10:07:34 +0000

    Started signup

--- src/Components/Signup.jsx
+++ src/Components/Signup.jsx
@@ -1,1 +1,18 @@
 import React from 'react';
+
+class SignUp extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      User: null,
+    }
+  }
+
+  render() {
+    return (
+      <div>Hello World</div>
+    )
+  }
+}
+
+export default SignUp;
