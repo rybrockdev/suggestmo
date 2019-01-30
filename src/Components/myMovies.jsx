1b1f4a588b5f1c811b51e6f084fde96b890962e1
Nevnet99 <luke-brannagan@hotmail.com>
2019-01-30 19:37:01 +0000

    Now the images show up

--- src/Components/myMovies.jsx
+++ src/Components/myMovies.jsx
@@ -7,6 +7,7 @@ class MyMovies extends React.Component {
     this.state = {
       movies: [],
       query: '',
+      images: [],
       mymovies: [],
     };
   }
@@ -19,7 +20,7 @@ class MyMovies extends React.Component {
       },
     })
       .then((response) => {
-        this.setState = ({
+        this.setState({
           movies: response.data.results,
         });
       });
@@ -31,23 +32,27 @@ class MyMovies extends React.Component {
     });
   };
 
+
   render() {
     return (
       <div className="mymovies">
         <h1>My Movies</h1>
         <input type="text" onChange={this.handleChange} placeholder="Search a title" />
         <button onClick={this.getFilm}>Submit</button>
-        {this.state.movies.map((movie) => {
-          return (
-            <div key={movie.title}>
-              <h1>{movie.title}</h1>
-            </div>
-          );
-        })}
-
         <div className="collection">
           <h1>Your collection</h1>
-
+          {
+            this.state.movies.length > 0 ? this.state.movies.map((movie, index) => {
+              return (
+                <div key={movie.id}>
+                  <h1>{movie.title}</h1>
+                  <div className="image" style={{ backgroundImage: `url( https://image.tmdb.org/t/p/w500/${movie.backdrop_path})` }} />
+                  <h1>{movie.release_date}</h1>
+                  <h1>{movie.vote_average}</h1>
+                </div>
+              );
+            }) : null
+        }
         </div>
       </div>
     );
